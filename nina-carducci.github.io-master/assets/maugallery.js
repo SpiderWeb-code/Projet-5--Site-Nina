43405172568e95aa71f99ed95d6ba3b532d46ad6
SpiderWeb-code <125570323+SpiderWeb-code@users.noreply.github.com>
2023-11-09 13:29:23 +0100

    Changement pour JavaScript + Légére Optimisation

--- nina-carducci.github.io-master/assets/maugallery.js
+++ nina-carducci.github.io-master/assets/maugallery.js
@@ -1,1 +1,152 @@
-!function(a){a.fn.mauGallery=function(t){t=a.extend(a.fn.mauGallery.defaults,t);var e=[];return this.each((function(){a.fn.mauGallery.methods.createRowWrapper(a(this)),t.lightBox&&a.fn.mauGallery.methods.createLightBox(a(this),t.lightboxId,t.navigation),a.fn.mauGallery.listeners(t),a(this).children(".gallery-item").each((function(l){a.fn.mauGallery.methods.responsiveImageItem(a(this)),a.fn.mauGallery.methods.moveItemInRowWrapper(a(this)),a.fn.mauGallery.methods.wrapItemInColumn(a(this),t.columns);var i=a(this).data("gallery-tag");t.showTags&&void 0!==i&&-1===e.indexOf(i)&&e.push(i)})),t.showTags&&a.fn.mauGallery.methods.showItemTags(a(this),t.tagsPosition,e),a(this).fadeIn(500)}))},a.fn.mauGallery.defaults={columns:3,lightBox:!0,lightboxId:null,showTags:!0,tagsPosition:"bottom",navigation:!0},a.fn.mauGallery.listeners=function(t){a(".gallery-item").on("click",(function(){t.lightBox&&"IMG"===a(this).prop("tagName")&&a.fn.mauGallery.methods.openLightBox(a(this),t.lightboxId)})),a(".gallery").on("click",".nav-link",a.fn.mauGallery.methods.filterByTag),a(".gallery").on("click",".mg-prev",(()=>a.fn.mauGallery.methods.prevImage(t.lightboxId))),a(".gallery").on("click",".mg-next",(()=>a.fn.mauGallery.methods.nextImage(t.lightboxId))),console.log(".mg-next"),console.log(".mg-prev")},a.fn.mauGallery.methods={createRowWrapper(a){a.children().first().hasClass("row")||a.append('<div class="gallery-items-row row"></div>')},wrapItemInColumn(a,t){if(t.constructor===Number)a.wrap(`<div class='item-column mb-4 col-${Math.ceil(12/t)}'></div>`);else if(t.constructor===Object){var e="";t.xs&&(e+=` col-${Math.ceil(12/t.xs)}`),t.sm&&(e+=` col-sm-${Math.ceil(12/t.sm)}`),t.md&&(e+=` col-md-${Math.ceil(12/t.md)}`),t.lg&&(e+=` col-lg-${Math.ceil(12/t.lg)}`),t.xl&&(e+=` col-xl-${Math.ceil(12/t.xl)}`),a.wrap(`<div class='item-column mb-4${e}'></div>`)}else console.error(`Columns should be defined as numbers or objects. ${typeof t} is not supported.`)},moveItemInRowWrapper(a){a.appendTo(".gallery-items-row")},responsiveImageItem(a){"IMG"===a.prop("tagName")&&a.addClass("img-fluid")},openLightBox(t,e){a(`#${e}`).find(".lightboxImage").attr("src",t.attr("src")),a(`#${e}`).modal("toggle")},prevImage(){let t=null;a("img.gallery-item").each((function(){a(this).attr("src")===a(".lightboxImage").attr("src")&&(t=a(this)),console.log(t)}));let e=a(".tags-bar span.active-tag").data("images-toggle"),l=[];"all"===e?a(".item-column").each((function(){a(this).children("img").length&&l.push(a(this).children("img"))})):a(".item-column").each((function(){a(this).children("img").data("gallery-tag")===e&&l.push(a(this).children("img"))}));let i=0,s=null;a(l).each((function(e){a(t).attr("src")===a(this).attr("src")&&(i=e-1)})),s=l[i]||l[l.length-1],a(".lightboxImage").attr("src",a(s).attr("src"))},nextImage(){let t=null;a("img.gallery-item").each((function(){a(this).attr("src")===a(".lightboxImage").attr("src")&&(t=a(this))}));let e=a(".tags-bar span.active-tag").data("images-toggle"),l=[];"all"===e?a(".item-column").each((function(){a(this).children("img").length&&l.push(a(this).children("img"))})):a(".item-column").each((function(){a(this).children("img").data("gallery-tag")===e&&l.push(a(this).children("img"))}));let i=0,s=null;a(l).each((function(e){a(t).attr("src")===a(this).attr("src")&&(i=e+1)})),s=l[i]||l[0],a(".lightboxImage").attr("src",a(s).attr("src"))},createLightBox(a,t,e){a.append(`<div class="modal fade" id="${t||"galleryLightbox"}" tabindex="-1" role="dialog" aria-hidden="true">\n                <div class="modal-dialog" role="document">\n                    <div class="modal-content">\n                        <div class="modal-body">\n                            ${e?'<div class="mg-prev" style="cursor:pointer;position:absolute;top:50%;left:-15px;background:white;"><</div>':'<span style="display:none;" />'}\n                            <img class="lightboxImage img-fluid" alt="Contenu de l'image affichée dans la modale au clique"/>\n                            ${e?'<div class="mg-next" style="cursor:pointer;position:absolute;top:50%;right:-15px;background:white;}">></div>':'<span style="display:none;" />'}\n                        </div>\n                    </div>\n                </div>\n            </div>`)},showItemTags(t,e,l){var i='<li class="nav-item"><span class="nav-link active active-tag"  data-images-toggle="all">Tous</span></li>';a.each(l,(function(a,t){i+=`<li class="nav-item active">\n                <span class="nav-link"  data-images-toggle="${t}">${t}</span></li>`}));var s=`<ul class="my-4 tags-bar nav nav-pills">${i}</ul>`;"bottom"===e?t.append(s):"top"===e?t.prepend(s):console.error(`Unknown tags position: ${e}`)},filterByTag(){if(!a(this).hasClass("active-tag")){a(".active-tag").removeClass("active active-tag"),a(this).addClass("active-tag");var t=a(this).data("images-toggle");a(".gallery-item").each((function(){a(this).parents(".item-column").hide(),("all"===t||a(this).data("gallery-tag")===t)&&a(this).parents(".item-column").show(300)}))}}}}(jQuery);
+///////////////////////////////////////////////////////////////////////  Création des boutons /////////////////////////////////////////////////////////////////////
+
+// Récupérez l'élément parent des boutons
+const boutonsContainer = document.getElementById("container-filter");
+
+// Tableau contenant les noms des boutons
+const nomsDesBoutons = ["Tous", "Concert", "Entreprises", "Mariages", "Portrait"];
+
+// Créez des boutons et ajoutez-les à la page en utilisant une boucle
+for (let i = 0; i < nomsDesBoutons.length; i++) {
+  const bouton = document.createElement("button");
+  bouton.textContent = nomsDesBoutons[i]; // Utilisez le nom du bouton comme texte
+  
+  // Créez un ID unique en combinant le nom du bouton et la valeur de i
+  bouton.id = `bouton-${nomsDesBoutons[i]}`;
+  
+  boutonsContainer.appendChild(bouton);
+}
+
+/////////////////////////////////////////////////////////////////////// Création des images ///////////////////////////////////////////////////////////////////////////
+
+// Récupérez l'élément parent des images
+const imagesContainer = document.getElementById("gallery-image");
+
+// Tableau contenant les chemins des images
+const cheminsDesImages = ["./assets/image/image-concert/concert-image.webp", "./assets/image/image-entreprise/entreprise-image.webp", "./assets/image/image-entreprise/deuxiéme-image-entreprise.webp", "./assets/image/image-mariage/mariage-image.webp", "./assets/image/image-portraits/image-portraits.webp", "./assets/image/image-mariage/deuxieme-image-mariage.webp", "./assets/image/image-portraits/deuxieme-image-portraits.webp", "./assets/image/image-concert/deuxième-image-concert.webp", "./assets/image/image-entreprise/troisiéme-image-entreprise.webp",];
+
+// Créez des images et ajoutez-les à la page en utilisant une boucle
+for (let i = 0; i < cheminsDesImages.length; i++) {
+  const image = document.createElement("img");
+  image.src = cheminsDesImages[i]; // Utilisez le chemin de l'image
+  // Créez un ID unique pour chaque image en utilisant la valeur de i
+  image.id = `image-${i}`;
+  image.className ="images_gallery"
+  imagesContainer.appendChild(image);
+}
+
+// Sélectionnez chaque élément par son ID
+const element1 = document.getElementById("image-0");
+const element2 = document.getElementById("image-1");
+const element3 = document.getElementById("image-2");
+const element4 = document.getElementById("image-3");
+const element5 = document.getElementById("image-4");
+const element6 = document.getElementById("image-5");
+const element7 = document.getElementById("image-6");
+const element8 = document.getElementById("image-7");
+const element9 = document.getElementById("image-8");
+////////////////////////////////////////////////////////////////////////////////////// Filtres les Images //////////////////////////////////////////////////////////////////////////
+const boutonTous = document.getElementById("bouton-Tous");
+function tous(){
+boutonTous.addEventListener("click", function (){
+    element1.style.display="initial";
+    element2.style.display="initial";
+    element3.style.display="initial";
+    element4.style.display="initial";
+    element5.style.display="initial";
+    element6.style.display="initial";
+    element7.style.display="initial";
+    element8.style.display="initial";
+    element9.style.display="initial";
+})
+};
+
+const boutonConcert = document.getElementById("bouton-Concert");
+function concert(){
+boutonConcert.addEventListener("click", function (){
+    element1.style.display="initial";
+    element2.style.display="none";
+    element3.style.display="none";
+    element4.style.display="none";
+    element5.style.display="none";
+    element6.style.display="none";
+    element7.style.display="none";
+    element8.style.display="initial";
+    element9.style.display="none";
+})
+};
+
+const boutonEntreprise = document.getElementById("bouton-Entreprises");
+function entreprise(){
+boutonEntreprise.addEventListener("click", function (){
+    element1.style.display="none";
+    element2.style.display="initial";
+    element3.style.display="initial";
+    element4.style.display="none";
+    element5.style.display="none";
+    element6.style.display="none";
+    element7.style.display="none";
+    element8.style.display="none";
+    element9.style.display="initial";
+})
+};
+
+const boutonMariage = document.getElementById("bouton-Mariages");
+function mariage(){
+boutonMariage.addEventListener("click", function (){
+    element1.style.display="none";
+    element2.style.display="none";
+    element3.style.display="none";
+    element4.style.display="initial";
+    element5.style.display="none";
+    element6.style.display="initial";
+    element7.style.display="none";
+    element8.style.display="none";
+    element9.style.display="none";
+})
+};
+
+const boutonPortrait = document.getElementById("bouton-Portrait");
+function portrait(){
+boutonPortrait.addEventListener("click", function (){
+    element1.style.display="none";
+    element2.style.display="none";
+    element3.style.display="none";
+    element4.style.display="none";
+    element5.style.display="initial";
+    element6.style.display="none";
+    element7.style.display="initial";
+    element8.style.display="none";
+    element9.style.display="none";
+})
+};
+if(boutonTous.click){
+  tous()
+}else{
+  console.log('Erreur: Le clique sur le bouton "Tous" n`a pas pu se faire');
+  alert('une erreur est survenue sur le bouton "Tous", si le probléme perciste, merci de contacter Sophie au *05 56 67 78 89*')
+}
+if(boutonConcert.click){
+  concert()
+}else{
+  console.log('Erreur: Le clique sur le bouton "Concert" n`a pas pu se faire');
+  alert('une erreur est survenue sur le bouton "Concert", si le probléme perciste, merci de contacter Sophie au *05 56 67 78 89*')
+}
+if(boutonEntreprise.click){
+  entreprise()
+}else{
+  console.log('Erreur: Le clique sur le bouton "Entreprise" n`a pas pu se faire');
+  alert('une erreur est survenue sur le bouton "Entreprise", si le probléme perciste, merci de contacter Sophie au *05 56 67 78 89*')
+}
+if(boutonMariage.click){
+  mariage()
+}else{
+  console.log('Erreur: Le clique sur le bouton "Mariage" n`a pas pu se faire');
+  alert('une erreur est survenue sur le bouton "Mariage", si le probléme perciste, merci de contacter Sophie au *05 56 67 78 89*')
+}
+if(boutonPortrait.click){
+  portrait()
+}else{
+  console.log('Erreur: Le clique sur le bouton "Portrait" n`a pas pu se faire');
+  alert('une erreur est survenue sur le bouton "Portrait", si le probléme perciste, merci de contacter Sophie au *05 56 67 78 89*')
+}
